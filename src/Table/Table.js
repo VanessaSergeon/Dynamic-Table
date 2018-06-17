ddfa04f456423a33a2451dd60b23b2f105948169
Vanessa Sergeon <vsergeon@gmail.com>
2018-06-17 13:42:55 -0400

    Add esdoc blocks

--- src/Table/Table.js
+++ src/Table/Table.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
 
 class Table extends Component {
+  /**
+   * this constructor sets the initial state of the Table componenet
+   * and binds functions to the 'this' context of the componenent.
+   * @param {object} props - props contains the table data to be rendered.
+   */ 
   constructor(props) {
     super(props);
 
     this.state = {
       sortedColumnIndex: null, // number
-      sortDirection: null, // asc or desc
+      sortDirection: null, // 'asc' or 'desc'
       tableData: this.props.tableData.slice(1)
     };
 
     this.sortAlgorithm = this.sortAlgorithm.bind(this);
   }
 
+  /**
+   * get the first array of table data that represnts the table header.
+   * @return {array} - array of the table columns.
+   */ 
   getHeaderData() {
     return this.props.tableData[0];
   }
 
+  /**
+   * @param {array} headerData - array of the table columns.
+   * @return - the html of the table header.
+   */ 
   TableHeader(headerData) {
     return (
       <thead>
@@ -35,6 +48,10 @@ class Table extends Component {
     );
   }
 
+  /**
+   * @param {number | null} columnIndex - the column index that is currently being sorted by.
+   * @return - the html of the table header sort icon button.
+   */
   getSortButton(columnIndex) {
     if (this.state.sortedColumnIndex === columnIndex && this.state.sortDirection === 'asc') {
       return (
@@ -51,31 +68,11 @@ class Table extends Component {
     }
   }
 
+  /**
+   * condidtional logic that determines which direction to sort the table in.
+   * @param {number | null} columnIndex - the column index that is currently being sorted by.
+   */
   sortColumn(columnIndex) {
-    // console.log(columnIndex)
-
-    // this.setState({sortedColumnIndex: columnIndex}, (columnIndex) => {
-    //   console.log('the function within the setState of the index happened')
-    //   // if the column index is the same
-    //     // sort direction is asc
-    //     // else reset index and ordering
-    //   if (this.state.sortedColumnIndex === columnIndex) {
-    //     if (this.state.sortDirection === 'asc') {
-    //       const reversedData = this.state.tableData.sort(this.sortAlgorithm).reverse();
-    //       this.setState({sortDirection: 'desc', tableData: reversedData});
-    //     } else if (this.state.sortDirection === 'desc') {
-    //       this.setState({sortDirection: null, sortedColumnIndex: null, tableData: this.props.tableData.slice(1)});
-    //     }
-    //   }
-
-    //   // if the column index is different
-    //     // sort ascending and set the column index
-    //   if (this.state.sortedColumnIndex !== columnIndex) {
-    //     this.setState({sortDirection: 'asc', sortedColumnIndex: columnIndex});
-    //     this.setState({tableData: this.state.tableData.sort(this.sortAlgorithm)});
-    //   }
-    // });
-
     // if the column index is the same
       // sort direction is asc
       // else reset index and ordering
@@ -98,27 +95,33 @@ class Table extends Component {
         const sortedData = this.state.tableData.sort(this.sortAlgorithm);
         this.setState({sortDirection: 'asc', tableData: sortedData});
       })
-
-      // this.setState({sortDirection: 'asc', sortedColumnIndex: columnIndex});
-      // this.setState({tableData: this.state.tableData.sort(this.sortAlgorithm)});
     }
-
-    // console.log(this.state.sortDirection);
-    // console.log(this.state.sortedColumnIndex);
   }
 
+  /**
+   * @param {number | string} a - the item the second item is being sorted against.
+   * @param {number | string} b - the item the first item is being sorted against.
+   * @return {number} - 1, 0, or -1.
+   */
   sortAlgorithm(a, b) {
-    console.log('the index when sorting happens', this.state.sortedColumnIndex);
     if (a[this.state.sortedColumnIndex] < b[this.state.sortedColumnIndex]) return -1;
     if (a[this.state.sortedColumnIndex] > b[this.state.sortedColumnIndex]) return 1;
     return 0;
   }
 
+  /**
+   * remove the first array of table data to leave only the body data.
+   * @return {array} - array of arrays representing the table body.
+   */ 
   getTableBodyData() {
     const bodyData = this.props.tableData.slice(1);
     return bodyData;
   }
 
+  /**
+   * @param {array} bodyData - array of arrays representing the table body.
+   * @return - the html of the table body.
+   */
   TableBody(bodyData) {
     return (
       <tbody>
@@ -129,6 +132,11 @@ class Table extends Component {
     );
   }
 
+  /**
+   * @param {array} rowData - array representing a table row.
+   * @param {number} index - number of the table row.
+   * @return - the html of a row in the table body.
+   */
   TableBodyRow(rowData, index) {
     return (
       <tr key={index}>
@@ -142,6 +150,9 @@ class Table extends Component {
     );
   }
 
+  /**
+   * render the componenent when new props are recieved and set the state.
+   */
   componentWillReceiveProps(nextProps) {
     this.setState({ tableData: nextProps.tableData.slice(1) });
   }
--- src/TableController/TableController.js
+++ src/TableController/TableController.js
@@ -4,6 +4,11 @@ import {table1, table2} from '../Table/TestData.js';
 import Table from '../Table/Table.js';
 
 class TableController extends Component {
+  /**
+   * this constructor sets the initial state of the TableController componenet
+   * and binds functions to the 'this' context of the componenent.
+   * @param {object} props - are not used for this component.
+   */
   constructor(props) {
     super(props);
 
@@ -13,14 +18,23 @@ class TableController extends Component {
     this.setTableData2 = this.setTableData2.bind(this);
   }
 
+  /**
+   * set the state to table1
+   */
   setTableData1() {
     this.setState({tableData: table1});
   }
 
+  /**
+   * set the state to table2
+   */
   setTableData2() {
     this.setState({tableData: table2});
   }
 
+  /**
+   * allow for switching of data set. render the table with the correct data set.
+   */
   render() {
     return (
       <div className="row">

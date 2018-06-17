4a9383ef7f9eea01c8caa5817c08bc06fd34d939
Vanessa Sergeon <vsergeon@gmail.com>
2018-06-17 13:49:08 -0400

    Add initial tests

--- src/Table/Table.test.js
+++ src/Table/Table.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Table from './Table';
+import {table1, table2} from './TestData.js';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Table tableData={table1}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
-
-
